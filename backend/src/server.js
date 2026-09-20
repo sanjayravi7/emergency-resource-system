865d933defa2fb578440627278b515db18440b6f
sanjay <sanjayravi861@gmail.com>
2026-09-21 02:10:49 +0530

    Backend is fully verified

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
-const app = require('./app');
-const { PORT } = require('./config/env');
+const app = require("./app");
+const env = require("./config/env");
 
-app.listen(PORT, () => {
-  console.log(Server running on port );
+app.listen(env.PORT, () => {
+  console.log(`Server running on port ${env.PORT}`);
 });
